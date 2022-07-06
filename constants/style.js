c2329f7b1e40118d7eb3ba27a1054957a0d4bd21
Tej <gurtej1992@gmail.com>
2022-07-06 05:18:41 -0400

    Working on welcome screen

---
+++ constants/style.js
@@ -0,0 +1,9 @@
+export const GlobalStyles = {
+    colors: {
+      primaryYellow: '#F8D925',
+      primaryRed: '#FF0000',
+      primaryRedDark: '#FF0000',
+      textColor: '#757575',
+      accent500: '#DEDEDE',
+    },
+  };
